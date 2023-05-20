15b9f4feb75386957dc77102552470fe424a98e3
vexra <112993677+vexra@users.noreply.github.com>
2023-05-20 14:13:52 +0700

    Update fibonacci.cpp

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -1,5 +1,29 @@
-int fib(int x) {
-   if ((x==1)||(x==0)) return x;
-   
-   return fib(x-1) + fib(x-2);
+// Buat kode program untuk mencari Bilangan Fibonacci dengan menggunakan teknik Rekursif dan Dynamic Programming, kemudian bandingkan running timenya.
+
+#include <iostream>
+using namespace std;
+
+long long fibonacciRecursive(long long val) {
+    if (val <=1) return val;
+    return fibonacciRecursive(val-1) + fibonacciRecursive(val-2);
+}
+
+long long fibonacciDynamicProgramming(long long val) {
+    long long dp[val+1];
+    dp[0] = 0;
+    dp[1] = 1;
+
+    for (long long i = 2; i <= val; ++i) {
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+
+    return dp[val];
+}
+
+int main()
+{
+    cout << fibonacciDynamicProgramming(50) << endl;
+    cout << fibonacciRecursive(50) << endl;
+
+    return 0;
 }
