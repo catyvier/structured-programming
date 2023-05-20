c45513a458c6d66f19c7d3db611fed65efdae885
catyvier <112993677+catyvier@users.noreply.github.com>
2023-05-20 21:40:07 +0700

    Update fibonacci.cpp

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -8,22 +8,20 @@ long long fibonacciRecursive(long long val) {
     return fibonacciRecursive(val-1) + fibonacciRecursive(val-2);
 }
 
-long long fibonacciDynamicProgramming(long long val) {
-    long long dp[val+1];
+long long fibonacciDynamicProgramming(int n) {
+    static long long dp[105] = {0};
     dp[0] = 0;
     dp[1] = 1;
 
-    for (long long i = 2; i <= val; ++i) {
-        dp[i] = dp[i-1] + dp[i-2];
+    if (dp[n] == 0) {
+        dp[n] = fibonacciDynamicProgramming(n-1) + fibonacciDynamicProgramming(n-2);
     }
 
-    return dp[val];
+    return dp[n];
 }
 
 int main()
 {
     cout << fibonacciDynamicProgramming(50) << endl;
-    cout << fibonacciRecursive(50) << endl;
-
     return 0;
 }
