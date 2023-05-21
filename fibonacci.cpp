7652e39951b792ebe7c7b9de9146c51b2a128c0a
vexra <112993677+vexra@users.noreply.github.com>
2023-05-21 12:58:55 +0700

    Update fibonacci.cpp

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -3,21 +3,20 @@
 #include <iostream>
 using namespace std;
 
-long long fibonacciRecursive(long long val) {
-    if (val <=1) return val;
-    return fibonacciRecursive(val-1) + fibonacciRecursive(val-2);
-}
-
 long long fibonacciDynamicProgramming(int n) {
-    static long long dp[105] = {0};
-    dp[0] = 0;
-    dp[1] = 1;
+    static int dp[105];
+    dp[0] = dp[1] = 1;
 
     if (dp[n] == 0) {
         dp[n] = fibonacciDynamicProgramming(n-1) + fibonacciDynamicProgramming(n-2);
     }
 
     return dp[n];
+}
+
+long long fibonacciRecursive(long long val) {
+    if (val <=1) return val;
+    return fibonacciRecursive(val-1) + fibonacciRecursive(val-2);
 }
 
 int main()
